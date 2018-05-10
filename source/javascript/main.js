69b06e31fc2f7ae1dbefc5b8011b6515ae70753d
Javier Forero <javierforero01@gmail.com>
2018-05-10 12:08:02 -0500

    added javascript

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -1,4 +1,4 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 
 $(document).ready(function(){
 
@@ -7,5 +7,39 @@ $(document).ready(function(){
     $('.mobile-drawer').toggleClass('mobile-drawer__slide');
     $('.mobile-drawer-menu li').toggleClass('mobile-drawer__fade');
   });
+
+  $('.menu-link').click(function(e){
+
+    var nameId = e.target.attributes['data-id'].value;
+    var mobile = e.target.attributes['data-mobile'].value;
+    switch(nameId) {
+      case 'about':
+        $('html,body').animate({
+          scrollTop: ($('#about').offset().top - $('.header').outerHeight())
+         }, 600);
+            break;  
+      case 'illinois':
+            $('html,body').animate({
+              scrollTop: ($('#illinois').offset().top - $('.header').outerHeight())
+            }, 600);
+            break;      
+      case 'involved':
+           $('html,body').animate({
+             scrollTop: ($('#involved').offset().top - $('.header').outerHeight())
+            }, 600);
+            break;     
+      case 'partners':
+           $('html,body').animate({
+             scrollTop: ($('#partners').offset().top - $('.header').outerHeight())
+           }, 600);
+            break;                 
+    }
+    if(mobile === "true") {
+      $('#mobile-icon').toggleClass('open');
+      $('.mobile-drawer').toggleClass('mobile-drawer__slide');
+      $('.mobile-drawer-menu li').toggleClass('mobile-drawer__fade');
+    }
+
+  })
 
 });
